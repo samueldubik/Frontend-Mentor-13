69b59b469d6586f1e5e661a4cb2a5facc9cfe8b7
Samuel Dubik <samuel.dubik@gmail.com>
2022-02-17 22:20:16 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'white'      : '#ffffff',
+      'dark-blue'  : '#090b1a',
+      'dark-purple': '#1b1938',
+      'pink'       : '#aa5cdb',
+
+    },
+
+    fontFamily: {
+      'inter' : ['Inter'],
+      'lexen' : ['Lexend Deca']
+    }
+  },
+  plugins: [],
+}
